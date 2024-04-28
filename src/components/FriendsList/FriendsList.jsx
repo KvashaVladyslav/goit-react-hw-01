dad84e6b8a5af939ef8473e591e7eebac7894023
KvashaVladyslav <146590289+KvashaVladyslav@users.noreply.github.com>
2024-04-28 15:55:08 +0200

    final

--- src/components/FriendsList/FriendsList.jsx
+++ src/components/FriendsList/FriendsList.jsx
@@ -1,12 +1,11 @@
-import friends from "/src/friends.json"
 import FriendsListItem from "../FriendsListItem/FriendsListItem";
 import clsx from "clsx";
 import css from "./FriendsList.module.css"
 
-export default function FriendsList() {
+export default function FriendsList({ friends }) {
     return (
     <ul className={clsx(css.list)}>
-        {friends.map(({avatar, name, isOnline, id}) => <li key={id}><FriendsListItem avatar={avatar} name={name} isOnline={isOnline} /></li>)}
+        {friends.map((friend) => <li key={friend.id}><FriendsListItem friend={friend} /></li>)}
     </ul>
     )
 }
--- src/components/FriendsListItem/FriendsListItem.jsx
+++ src/components/FriendsListItem/FriendsListItem.jsx
@@ -1,7 +1,7 @@
 import css from "./FriendsListItem.module.css"
 import clsx from 'clsx'
 
-export default function FriendsListItem({ avatar, name, isOnline }) {
+export default function FriendsListItem({ friend: {isOnline, avatar, name} }) {
     return (
     <div className={clsx(css.item, isOnline === true ? css.onlineBorder : css.offlineBorder)}>
         <img className={clsx(css.image)} src={avatar} alt="Avatar" width="48" />
