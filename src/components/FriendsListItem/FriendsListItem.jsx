44d71dded24afcc5f0103459735779d37e1ff986
KvashaVladyslav <146590289+KvashaVladyslav@users.noreply.github.com>
2024-04-29 21:07:01 +0200

    upp

--- src/components/FriendsListItem/FriendsListItem.jsx
+++ src/components/FriendsListItem/FriendsListItem.jsx
@@ -6,7 +6,7 @@ export default function FriendsListItem({ friend: {isOnline, avatar, name} }) {
     <div className={clsx(css.item, isOnline === true ? css.onlineBorder : css.offlineBorder)}>
         <img className={clsx(css.image)} src={avatar} alt="Avatar" width="48" />
         <p className={clsx(css.name)}>{name}</p>
-        <p className={clsx (css.text, isOnline === true ? css.online : css.offline) }>{isOnline?'Online':'Offline'}</p>
+        <p className={clsx (css.text, isOnline === true ? css.online : css.offline) }>{isOnline ? 'Online' : 'Offline'}</p>
     </div>
     )
 } 
